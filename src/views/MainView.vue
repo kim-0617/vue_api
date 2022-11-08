ec5ba855d793c273af7ed78a6ad523221151f5b5
kim-0617 <kkk5993@naver.com>
2022-11-08 11:24:47 +0900

    22.11.08

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
     <HeaderCont />
-    <TitleCont />
-    <ContactCont />
     <FooterCont />
   </div>
 </template>
@@ -10,13 +8,9 @@
 <script>
 import HeaderCont from "@/components/HeaderCont.vue";
 import FooterCont from "@/components/FooterCont.vue";
-import TitleCont from "@/components/TitleCont.vue";
-import ContactCont from "@/components/ContactCont.vue";
 export default {
   components: {
     HeaderCont,
-    TitleCont,
-    ContactCont,
     FooterCont,
   },
 };
